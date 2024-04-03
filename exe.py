0ac054a72c57e6faed3406f8b7a8bd86361cfd68
Your Name <sarveenb2002@outlook.com>
2024-04-03 12:42:56 +0530

    example1

--- exe.py
+++ exe.py
@@ -1,1 +1,36 @@
-print("wlecome")
+a="welcome to azure family"
+l=len(a)
+print(l)
+
+lower=a.lower()
+print(lower)
+
+upper=a.upper()
+print(upper)
+
+s=a[1:3]
+print(s)
+
+s=a[4:9]
+print(s)
+
+s=a[11:12]
+print(s)
+
+s=a[ :3]
+print(s)
+
+s=a[4: ]
+print(s)
+
+s=a[1:10:2]
+print(s)
+
+s=a[ :10:3]
+print(s)
+
+
+
+
+
+
---
+++ exe1.py
@@ -0,0 +1,29 @@
+#UPPERcase FN
+
+my_var="sarveen"
+var_1=my_var.upper()
+print(var_1)
+
+#LOWERCASE FN
+
+my_var="SARVEEN"
+var_1=my_var.lower()
+print(var_1)
+
+#LENGTH FN
+
+a="welcome to azure"
+a=len(a)
+print(a)
+
+#append
+
+b=[]
+a=b.append("a""gitb")
+print(b)
+
+#slicing
+
+a="welcome"
+s=a[0:4]
+print(s)
